eb5db1cece35558a2aa57f8edc11874e900518b6
Matteo Johannes Burgler <mburgler@c2r6p2.42vienna.com>
2023-12-27 19:12:59 +0100

    upload

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mburgler <mburgler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/27 18:59:05 by mburgler          #+#    #+#             */
+/*   Updated: 2023/12/27 19:04:29 by mburgler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+Class	Zombie {
+
+	private:
+
+		std::string name;
+	public:
+		void	announce( void );
+
+
+};
+
+#endif
